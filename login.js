08bdaf4bf1b7970301be45a4055226e69887bca0
krder5k <44813747+krder5k@users.noreply.github.com>
2024-05-09 17:14:12 +0200

    Update login.js
    added )};

--- login.js
+++ login.js
@@ -3,36 +3,36 @@ function login() {
     const password = document.getElementById('password').value;
 
     // Send a request to your server
-    fetch('https://bodystate.azurewebsites.net/login', { // Use the correct route for the login endpoint
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
+fetch('https://bodystate.azurewebsites.net/login', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
         username: document.getElementById('username').value,
         password: document.getElementById('password').value
     })
-    .then(response => response.json())
-    .then(data => {
-        console.log(data); // Эта строка добавлена для отладки
-        if(data.success) {
-            localStorage.setItem('stepsUrl', data.stepsUrl);
-            console.log('Setting localStorage items...');
-            console.log('Saved stepsUrl:', data.stepsUrl);
-            localStorage.setItem('sleepUrl', data.sleepUrl);
-            console.log('Saved sleepUrl:', data.sleepUrl);
-            localStorage.setItem('fiwareService', data.fiwareService);
-            localStorage.setItem('fiwareServicePath', data.fiwareServicePath);
-            window.location.href = '/index.html';
-        } else {
-            // Handle login failure
-            alert('Login failed!');
-        }
-    })
-    .catch((error) => {
-        console.error('Error:', error);
-    });
-}
+})
+.then(response => response.json())
+.then(data => {
+    console.log(data); // Эта строка добавлена для отладки
+    if(data.success) {
+        localStorage.setItem('stepsUrl', data.stepsUrl);
+        console.log('Setting localStorage items...');
+        console.log('Saved stepsUrl:', data.stepsUrl);
+        localStorage.setItem('sleepUrl', data.sleepUrl);
+        console.log('Saved sleepUrl:', data.sleepUrl);
+        localStorage.setItem('fiwareService', data.fiwareService);
+        localStorage.setItem('fiwareServicePath', data.fiwareServicePath);
+        window.location.href = '/index.html';
+    } else {
+        // Handle login failure
+        alert('Login failed!');
+    }
+})
+.catch((error) => {
+    console.error('Error:', error);
+});
 
 
 // function login() {
