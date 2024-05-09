d21260b94eff91cb9b329d384567386dce9a354f
krder5k <44813747+krder5k@users.noreply.github.com>
2024-05-09 11:43:10 +0200

    Update login.js
    Redirect to index.html

--- login.js
+++ login.js
@@ -3,7 +3,7 @@ function login() {
     const password = document.getElementById('password').value;
 
     // Send a request to your server
-    fetch('http://localhost:8080/login', { // Use the correct route for the login endpoint
+    fetch('https://bodystate.azurewebsites.net/login.html', { // Use the correct route for the login endpoint
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
@@ -21,7 +21,7 @@ function login() {
             console.log('Saved sleepUrl:', data.sleepUrl);
             localStorage.setItem('fiwareService', data.fiwareService);
             localStorage.setItem('fiwareServicePath', data.fiwareServicePath);
-            window.location.href = 'http://localhost:8080/';
+            window.location.href = '/index.html';
         } else {
             // Handle login failure
             alert('Login failed!');
