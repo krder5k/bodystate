1281202b7b424a9e3331f49263d907ff188db201
krder5k <44813747+krder5k@users.noreply.github.com>
2024-05-10 12:38:00 +0200

    Update login.js
    Better error handling

--- login.js
+++ login.js
@@ -2,27 +2,36 @@ function login() {
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
 
-    // Send a request to your server
-fetch('https://bodystate.azurewebsites.net/login', {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json',
-    },
-body: JSON.stringify({ username, password })
-})
-.then(response => response.json())
-.then(data => {
-    console.log(data); // Эта строка добавлена для отладки
-    if(data.success) {
-        localStorage.setItem('stepsUrl', data.stepsUrl);
-        console.log('Setting localStorage items...');
-        console.log('Saved stepsUrl:', data.stepsUrl);
-        localStorage.setItem('sleepUrl', data.sleepUrl);
-        console.log('Saved sleepUrl:', data.sleepUrl);
-        localStorage.setItem('fiwareService', data.fiwareService);
-        localStorage.setItem('fiwareServicePath', data.fiwareServicePath);
-        window.location.href = 'https://bodystate.azurewebsites.net';
-    } else {
+    fetch('https://bodystate.azurewebsites.net/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, password })
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        if(data.success) {
+            localStorage.setItem('stepsUrl', data.stepsUrl);
+            localStorage.setItem('sleepUrl', data.sleepUrl);
+            localStorage.setItem('fiwareService', data.fiwareService);
+            localStorage.setItem('fiwareServicePath', data.fiwareServicePath);
+            window.location.href = 'https://bodystate.azurewebsites.net';
+        } else {
+            alert('Login failed: ' + data.message);
+        }
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+        alert('Login failed. Please check the console for more information.');
+    });
+}
+
         // Handle login failure
         alert('Login failed!');
     }
